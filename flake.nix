f08e91444356681fd36ddbae43f7910410b5771c
Delaney Gillilan <delaneygillilan@gmail.com>
2026-07-22 22:08:49 -0700

    chore: bump version to v0.0.9

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
       checks = forAllSystems (pkgs: {
         default = pkgs.buildGo126Module {
           pname = "ron-go-tests";
-          version = "0.0.8";
+          version = "0.0.9";
           src = self;
           vendorHash = "sha256-Q9o76rYrQauzcyHUEtPbfuSWtzG5sMu0zYhaQ7I03hI=";
           proxyVendor = true;
